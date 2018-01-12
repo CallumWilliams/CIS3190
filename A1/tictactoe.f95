2b3bc43fb1c3e36becdec7427b2683ca06a78fb7
Callum Williams <callum.a.williams@gmail.com>
2018-01-12 13:39:43 -0500

    basic user input

--- A1/tictactoe.f95
+++ A1/tictactoe.f95
@@ -6,17 +6,39 @@ program tictactoe
 
 implicit none
 
-character(len = 9) :: board = '         ' !blank board
-logical :: tmp
-integer  :: gameState = 1;
-
-tmp = showBoard(board)
-
-do
+call playTicTacToe()
 
 contains
 
-logical function showBoard(b)
+subroutine playTicTacToe()
+	
+	character(len=9) :: b = '         ' !blank board
+	integer :: gameState = 1
+	integer :: playerMove;
+	integer :: compMove;
+	
+	write(*,*) 'TicTacToe'
+	write(*,*) '---------'
+	write(*,*) ' 1 | 2 | 3 '
+	write(*,*) '---|---|---'
+	write(*,*) ' 4 | 5 | 6 '
+	write(*,*) '---|---|---'
+	write(*,*) ' 7 | 8 | 9 '
+	write(*,*)
+	
+	do
+		
+		playerMove = getMove(b)
+		b(playerMove:playerMove) = 'X'
+		call showBoard(b)
+		
+		if (gameState == 0) exit
+		
+	end do
+	
+end subroutine playTicTacToe
+
+subroutine showBoard(b)
 	
 	implicit none
 	
@@ -29,6 +51,38 @@ logical function showBoard(b)
 		if (i /= 3) write(*,*) '-+-+-'
 	end do
 	
-end function showBoard
+end subroutine showBoard
+
+integer function getMove(b)
+	
+	character(len=9), intent(in) :: b
+	integer :: playerMove
+	
+	do
+		write(*,*) 'Your move: '
+		read(*,50) playerMove
+		50 format(I1)
+		if (chkplay(b, playerMove) == 1) then
+			exit
+		else
+			write(*,*) 'Move invalid.'
+		end if
+	end do
+	getMove = playerMove
+	
+end function getMove
+
+integer function chkplay(b, p)
+	
+	character(len=9), intent(in) :: b
+	integer, intent(in) :: p
+	
+	if (b(p:p) /= ' ') then
+		chkplay = 0
+	else
+		chkplay = 1
+	end if
+	
+end function chkplay
 
 end program tictactoe
