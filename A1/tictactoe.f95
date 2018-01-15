5ab1e36c936f2a38a1bc712cb0d42f799212cfea
Callum Williams <callum.a.williams@gmail.com>
2018-01-15 12:51:46 -0500

    Added computer decision algorithm

--- A1/tictactoe.f95
+++ A1/tictactoe.f95
@@ -31,12 +31,163 @@ subroutine playTicTacToe()
 		playerMove = getMove(b)
 		b(playerMove:playerMove) = 'X'
 		call showBoard(b)
+		compMove = pickMove(b)
+		write(*,*) compMove
+		b(compMove:compMove) = 'O'
+		call showBoard(b)
 		
 		if (gameState == 0) exit
 		
 	end do
 	
 end subroutine playTicTacToe
+
+integer function pickMove(b)
+	
+	character(len=9), intent(in) :: b
+	integer :: checkSum = 0
+	integer :: checkComplete = 0
+	integer :: i
+	integer :: output = 0
+	
+	!first row
+	do i = 1, 3
+		if (b(i:i) == 'O') then
+			checkSum = checkSum + 4
+		else if (b(i:i) == 'X') then
+			checkSum = checkSum + 1
+		else
+			output = i !in case either of the two success cases happen, keep track of blank space
+		end if
+	end do
+	if (checkSum == 8 .or. checkSum == 2) then
+		pickMove = output
+		go to 20
+	end if
+	
+	checkSum = 0
+	!second row
+	do i = 4, 6
+		if (b(i:i) == 'O') then
+			checkSum = checkSum + 4
+		else if (b(i:i) == 'X') then
+			checkSum = checkSum + 1
+		else
+			output = i !if either of the two success cases happen, keep track of blank space
+		end if
+	end do
+	if (checkSum == 8 .or. checkSum == 2) then
+		pickMove = output
+		go to 20
+	end if
+	
+	checkSum = 0
+	!third row
+	do i = 7, 9
+		if (b(i:i) == 'O') then
+			checkSum = checkSum + 4
+		else if (b(i:i) == 'X') then
+			checkSum = checkSum + 1
+		else
+			output = i !if either of the two success cases happen, keep track of blank space
+		end if
+	end do
+	if (checkSum == 8 .or. checkSum == 2) then
+		pickMove = output
+		go to 20
+	end if
+	
+	checkSum = 0
+	!first column
+	do i = 1, 7, 3
+		if (b(i:i) == 'O') then
+			checkSum = checkSum + 4
+		else if (b(i:i) == 'X') then
+			checkSum = checkSum + 1
+		else
+			output = i !if either of the two success cases happen, keep track of blank space
+		end if
+	end do
+	if (checkSum == 8 .or. checkSum == 2) then
+		pickMove = output
+		go to 20
+	end if
+	
+	checkSum = 0
+	!second column
+	do i = 2, 8, 3
+		if (b(i:i) == 'O') then
+			checkSum = checkSum + 4
+		else if (b(i:i) == 'X') then
+			checkSum = checkSum + 1
+		else
+			output = i !if either of the two success cases happen, keep track of blank space
+		end if
+	end do
+	if (checkSum == 8 .or. checkSum == 2) then
+		pickMove = output
+		go to 20
+	end if
+	
+	checkSum = 0
+	!third column
+	do i = 3, 9, 3
+		if (b(i:i) == 'O') then
+			checkSum = checkSum + 4
+		else if (b(i:i) == 'X') then
+			checkSum = checkSum + 1
+		else
+			output = i !if either of the two success cases happen, keep track of blank space
+		end if
+	end do
+	if (checkSum == 8 .or. checkSum == 2) then
+		pickMove = output
+		go to 20
+	end if
+	
+	checkSum = 0
+	!first diagonal
+	do i = 1, 9, 4
+		if (b(i:i) == 'O') then
+			checkSum = checkSum + 4
+		else if (b(i:i) == 'X') then
+			checkSum = checkSum + 1
+		else
+			output = i !if either of the two success cases happen, keep track of blank space
+		end if
+	end do
+	if (checkSum == 8 .or. checkSum == 2) then
+		pickMove = output
+		go to 20
+	end if
+	
+	checkSum = 0
+	!second diagonal
+	do i = 3, 7, 2
+		if (b(i:i) == 'O') then
+			checkSum = checkSum + 4
+		else if (b(i:i) == 'X') then
+			checkSum = checkSum + 1
+		else
+			output = i !if either of the two success cases happen, keep track of blank space
+		end if
+	end do
+	if (checkSum == 8 .or. checkSum == 2) then
+		pickMove = output
+		go to 20
+	end if
+	
+	!pick randomly
+	do
+		output = int(rand()*9)+1
+		if (b(output:output) == ' ') exit
+		write(*,*) 'rand'
+	end do
+	
+	20 write(*,*) 'Opponent move'
+	pickMove = output
+	
+end function pickMove
 
 subroutine showBoard(b)
 	
