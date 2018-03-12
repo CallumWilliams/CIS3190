c913478a650b54d3fbf637c895fd3fa4ff48a9fd
Callum Williams <callum.a.williams@gmail.com>
2018-03-12 12:56:30 -0400

    More learning

--- A3/conv.cob
+++ A3/conv.cob
@@ -1,11 +1,17 @@
 program-id. conv.
 
-environment division.
-
 data division.
 	working-storage section.
-		01 current pic XX.
-
+		77 usr-in pic x(30) value space.
+		77 str-len pic 99.
+		77 ind pic 99.
+		77 current-char pic x.
+		
 procedure division.
-	move 't' to current
-	display 'Variable |', current, '|'.
+	display 'Enter the Roman Numeral to be converted: '.
+	accept usr-in.
+	unstring usr-in
+		delimited by all spaces
+		into usr-in count in str-len.
+	display str-len.
+		
