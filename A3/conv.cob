0773352927d44ed43a67e8c3f8fbb63609ee619e
Callum Williams <callum.a.williams@gmail.com>
2018-03-13 13:41:48 -0400

    File reading

--- A3/conv.cob
+++ A3/conv.cob
@@ -1,17 +1,35 @@
 program-id. conv.
 
+environment division.
+	input-output section.
+		file-control.
+			select stdin assign to "input.txt" organization is line sequential.
+			select stdout assign to display.
+
 data division.
+	file section.
+		fd stdin.
+			01 in-line pic x(30).
 	working-storage section.
-		77 usr-in pic x(30) value space.
-		77 str-len pic 99.
-		77 ind pic 99.
-		77 current-char pic x.
+		77 eof pic 9 value 1.
+		77 sum pic 999.
 		
 procedure division.
 	display 'Enter the Roman Numeral to be converted: '.
-	accept usr-in.
-	unstring usr-in
-		delimited by all spaces
-		into usr-in count in str-len.
-	display str-len.
+	
+	open input stdin.
+	
+	perform readLine until eof = 0.
+	
+	close stdin.
+	
+	stop run.
+	
+	readLine.
+		read stdin into in-line at end move zero to eof.
 		
+		if eof is not equal to zero
+			display in-line.
+	
+	calculateValue.
+		move 0 to sum.
