fa18726764d15846172ddc8031b45f1efa79591e
Callum Williams <callum.a.williams@gmail.com>
2018-03-14 01:53:41 -0400

    Very simple parsing

--- A3/conv.cob
+++ A3/conv.cob
@@ -11,14 +11,17 @@ data division.
 		fd stdin.
 			01 in-line pic x(30).
 	working-storage section.
+		01 parsing.
+			05 initial-parse pic 9 value 1.
+			05 magnitude pic 9.
 		77 eof pic 9 value 1.
 		77 err pic 9 value 1.
 		77 strlen pic 99.
 		77 i pic 9 value 1.
-		77 total pic 999.
+		77 total pic 999999.
+		77 current pic x.
 		
 procedure division.
-	display 'Enter the Roman Numeral to be converted: '.
 	
 	open input stdin.
 	
@@ -41,8 +44,65 @@ procedure division.
 			into in-line count in strlen.
 		move 0 to total.
 		move 1 to i.
+		move 1 to initial-parse.
 		perform calc-loop until i is greater than strlen.
+		display in-line, ' = ', total.
 		
 		calc-loop.
-			display in-line(i:1).
+			move in-line(i:1) to current.
+			if current is equal to 'M' or 'm'
+				
+				perform addM
+				
+			else if current is equal to 'D' or 'd'
+				
+				perform addD
+					
+			else if current is equal to 'C' or 'c'
+				
+				perform addC
+				
+			else if current is equal to 'L' or 'l'
+				
+				perform addL
+				
+			else if current is equal to 'X' or 'x'
+				
+				perform addX
+				
+			else if current is equal to 'V' or 'v'
+				
+				perform addV
+				
+			else if current is equal to 'I' or 'i'
+				
+				perform addI
+				
+			else
+				
+				display 'error'.
+				
 			add 1 to i.
+			display total.
+		
+		addM.
+			add 1000 to total.
+			
+		addD.
+			add 500 to total.
+			
+		addC.
+			add 100 to total.
+			
+		addL.
+			add 50 to total.
+			
+		addX.
+			add 10 to total.
+			
+		addV.
+			add 5 to total.
+			
+		addI.
+			add 1 to total.
+			
