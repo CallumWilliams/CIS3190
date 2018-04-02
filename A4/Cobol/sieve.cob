ce90e932092cbbd2d716e129e273495e8267cd0a
Callum Williams <callum.a.williams@gmail.com>
2018-04-01 21:47:16 -0400

    Cobol Sieve

--- A4/Cobol/sieve.cob
+++ A4/Cobol/sieve.cob
@@ -20,6 +20,11 @@ data division.
 			02 val pic 999999 occurs 1 to 999999 times depending on N.
 		01 prompt-msg.
 			02 filler pic x(36) value is 'Please enter the size of the sieve: '.
+		01 sieve.
+			02 sieve-div pic 9 occurs 4 times.
+			02 sieve-ind pic 9.
+			02 check pic 999999.
+			02 rem pic 9.
 			
 procedure division.
 	display prompt-msg.
@@ -28,17 +33,40 @@ procedure division.
 	
 	perform initialize-values until counter is equal to N.
 	
-	move 1 to counter.
+	move 2 to sieve-div(1).
+	move 3 to sieve-div(2).
+	move 5 to sieve-div(3).
+	move 7 to sieve-div(4).
+	move 3 to counter.
+	perform perform-sieve until counter is equal to N.
 	
+	move 1 to counter.
+	open output standard-output.
 	perform print-values until counter is equal to N.
+	close standard-output.
+	
+	stop run.
 	
 	initialize-values.
 		move counter to val(counter).
 		add 1 to val(counter).
 		add 1 to counter.
+	
+	perform-sieve.
+		move 1 to sieve-ind.
+		perform apply-sieve until sieve-ind is greater than 4.
+		add 1 to counter.
+	
+	apply-sieve.
+		divide val(counter) by sieve-div(sieve-ind) giving check remainder rem.
+		if check is not equal to 0 or 1
+			if rem is equal to 0
+				move 0 to val(counter)
+				move 4 to sieve-ind. 
+		add 1 to sieve-ind.
 		
 	print-values.
 		if val(counter) is not equal to 0
-			display val(counter).
+			write stdout from val(counter) after advancing 1 line.
 		add 1 to counter.
 	
